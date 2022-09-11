679fb485b3dc5d782ea60966bacdccc86a375146
mjb <mojbashtani@gmail.com>
2022-09-11 15:59:35 +0430

    Create Location and Person models

---
+++ Favourite Location/Map feature/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  Favourite Location
+//
+//  Created by Mohammad Javad Bashtani on 6/20/1401 AP.
+//
+
+import Foundation
+
+struct Location {
+    let latitude: Double
+    let longitude: Double
+}
---
+++ Favourite Location/Map feature/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  Favourite Location
+//
+//  Created by Mohammad Javad Bashtani on 6/20/1401 AP.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    
+}
