a160d3e898b544114b5403f8979292bfe2a0f26c
mjb <mojbashtani@gmail.com>
2022-09-11 15:42:21 +0430

    Initial Commit

---
+++ Favourite Location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Favourite Location
+//
+//  Created by Mohammad Javad Bashtani on 6/20/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
