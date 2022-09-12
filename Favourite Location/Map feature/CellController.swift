627feb4b86c1b40136be4da1bacac884e0512d97
mjb <mojbashtani@gmail.com>
2022-09-12 16:18:13 +0430

    Add generic cell controller

---
+++ Favourite Location/Map feature/CellController.swift
@@ -0,0 +1,34 @@
+//
+//  CellController.swift
+//  Favourite Location
+//
+//  Created by Mohammad Javad Bashtani on 6/21/1401 AP.
+//
+
+import UIKit
+
+public struct CellController {
+    let id: AnyHashable
+    let dataSource: UITableViewDataSource
+    let delegate: UITableViewDelegate?
+    let dataSourcePrefetching: UITableViewDataSourcePrefetching?
+    
+    public init(id: AnyHashable, _ dataSource: UITableViewDataSource) {
+        self.id = id
+        self.dataSource = dataSource
+        self.delegate = dataSource as? UITableViewDelegate
+        self.dataSourcePrefetching = dataSource as? UITableViewDataSourcePrefetching
+    }
+}
+
+extension CellController: Equatable {
+    public static func == (lhs: CellController, rhs: CellController) -> Bool {
+        lhs.id == rhs.id
+    }
+}
+
+extension CellController: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
