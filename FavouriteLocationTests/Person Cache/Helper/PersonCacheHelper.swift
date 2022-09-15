2b243c43a1da32ffc3e045407e949325b93e5037
Mohamamad Bashtani <mohamadjavadbashtani@gmail.com>
2022-09-15 22:25:56 +0430

    Update swift.yml

--- FavouriteLocationTests/Person Cache/Helper/PersonCacheHelper.swift
+++ FavouriteLocationTests/Person Cache/Helper/PersonCacheHelper.swift
@@ -6,20 +6,22 @@
 //
 
 import Foundation
-@testable import Favourite_Location
+@testable import FavouriteLocation
 
 func anyNSError() -> NSError {
     return NSError(domain: "any error", code: 0)
 }
 
 func uniquePerson() -> Person {
-    .init(id: UUID().uuidString, firstName: "", lastName: "", location: .init(latitude: 0, longitude: 0))
+    .init(id: UUID().uuidString, firstName: "", lastName: "", locations: [.init(latitude: 0, longitude: 0)])
 }
 
 func uniquePersons() -> (local: [LocalPerson], models: [Person]) {
     let persons = [uniquePerson()]
     return (persons.map {
-        .init(id: $0.id, firstName: $0.firstName, lastName: $0.lastName, location: .init(latitude: $0.location.latitude, longitude: $0.location.longitude))
+        .init(id: $0.id, firstName: $0.firstName, lastName: $0.lastName, locations: $0.locations.map {
+            .init(latitude: $0.latitude, longitude: $0.longitude)
+        })
     }, persons)
 }
 
